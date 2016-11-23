1a847faf7cba4c6cb92b585237b81784efc6c760
venkatachalamd <venkat@smartglossa.com>
2016-11-23 15:08:06 +0530

    backend coding completed

--- questionanswer/src/main/java/com/smartglossa/questionanswer/QuestionAnswerServlet.java
+++ questionanswer/src/main/java/com/smartglossa/questionanswer/QuestionAnswerServlet.java
@@ -1,39 +1,46 @@
 package com.smartglossa.questionanswer;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class QuestionAnswerServlet
- */
 public class QuestionAnswerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public QuestionAnswerServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public QuestionAnswerServlet() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String operation = request.getParameter("operation");
+		String question = request.getParameter("ques");
+		String answer = request.getParameter("ans");
+		if(operation.equals("add")){
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/qa", "root", "root");
+				Statement stmt = conn.createStatement();
+				String query = "insert into question(question,answer) values('" + question + "','" + answer + "')";
+				stmt.execute(query);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		
 	}
 
 }
