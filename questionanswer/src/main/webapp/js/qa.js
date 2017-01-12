be93e592af7b5b8a09dae4067e2454e7b37c9250
parameswaris <parameswari@smartglossa.com>
2017-01-12 17:20:05 +0530

    create file

---
+++ questionanswer/src/main/webapp/js/qa.js
@@ -0,0 +1,34 @@
+function question(){
+	var user="";
+user += "<table>";
+user += "<tr><td>UserId*:<\/td><td><input type=\"text\"id=\"userid\"placeholder=\"UserId!..\"><\/td><\/tr>";
+user += "<tr><td>Name*:<\/td><td><input type=\"text\"id=\"name\"placeholder=\"Name!..\"><\/td><\/tr>";
+user += "<tr><td>UserName*:<\/td><td><input type=\"text\"id=\"uname\"placeholder=\"UserName!..\"><\/td><\/tr>";
+user += "<tr><td>Password*:<\/td><td><input type=\"password\"id=\"pass\"placeholder=\"Password!..\"><\/td><\/tr>";
+user += "<tr><td><button id=\"submit\">Submit<\/button><\/td><td><button id=\"update\">Update<\/button><\/td><\/tr>";
+user += "<\/table>";
+$('.use')[0].innerHTML=user;
+}
+function qa(){
+	var ques="";
+	ques += "<table>";
+	ques += "<tr><td>QuestionId*:<td><td><input type=\"text\"id=\"qusId\"placeholder=\"QuestionId!..\"><\/td><\/tr>";
+	ques += "<tr><td>Question*:<td><td><input type=\"text\"id=\"ques\"placeholder=\"Question!..\"><\/td><\/tr>";
+	ques += "<tr><td>UserName*:<td><td><input type=\"text\"id=\"uname\"placeholder=\"UserName!..\"><\/td><\/tr>";
+	ques += "<tr><td>Date*:<td><td><input type=\"text\"id=\"date\"placeholder=\"DateTime!..\"><\/td><\/tr>";
+	ques += "<tr><td><button id=\"submit\">Submit<\/button><\/td><\/tr>";
+	ques += "<\/table>";
+   $('.question')[0].innerHTML=ques;
+}
+
+function answer(){
+	var ans="";
+	ans += "<table>";
+	ans += "<tr><td>AnswerId*:<\/td><td><input id=\"id\"placeholder=\"AnswerId!..\"><\/td><\/tr>";
+	ans += "<tr><td>Answer*:<\/td><td><input id=\"ans\"placeholder=\"Answer!..\"><\/td><\/tr>";
+	ans += "<tr><td>UserName*:<\/td><td><input id=\"uname\"placeholder=\"UserName!..\"><\/td><\/tr>";
+	ans += "<tr><td>Answerdate*:<\/td><td><input id=\"adate\"placeholder=\"adate!..\"><\/td><\/tr>";
+	ans += "<tr><td><button id=\"sub\">Submit<\/button><\/td><\/tr>";
+	ans += "<\/table>";
+$('.answ')[0].innerHTML=ans;
+}
--- questionanswer/src/main/webapp/js/question.js
+++ questionanswer/src/main/webapp/js/question.js
@@ -1,45 +1,148 @@
 $(document).ready(function(){
-	getAll();
 	$(document).on("click","#submit",function(){
-		var ques = $("#qus").val();
-		var ans = $("#an").val();
-		var url = "http://localhost:8080/questionanswer/question?operation=add&ques="+ ques +"&ans="+ ans;
+		var ques=$('#ques').val();
+		var uname=$('#uname').val();
+		var date=$('#date').val();
+		if(ques==""){
+        alert("please enter Question");
+        return;
+		}
+		if(uname==""){
+			alert("please enter your UserName");
+			return;
+		}
+		
+		if(date==""){
+			alert("please enter your Date");
+			return;
+		}
+		var url="/questionanswer/question?operation=add&ques="+ques+"&uName="+uname+"&date="+date;
+		$('input[type=text]').val();
 		$.ajax({
-			url:url,
-			type:'POST'
-		}).done(function(result){
-			
-			getAll();
-		})
-		.fail(function(){
-			alert(result);
-		})
-	});
-	$(document).on('click','.delete',function(){
-		var tr = $(this).parent().parent();
-		var qid = tr.children(".qid")[0].innerHTML;
-		var url = "http://localhost:8080/questionanswer/question?operation=delete&qid="+ qid;
-		$.ajax({
-			url:url,
-			type:'POST'
+			url: url,
+			type:'post'
 		})
 		.done(function(result){
-		   getAll();
-		   
-		})
+				alert("Successfully Added");
+			})
+		.fail(function(result){
+			alert("Error Occurs");
+		});
 	});
-	$(document).on('keyup','#qid',function(){
-		var qid = $("#qid").val();
-		var url = "http://localhost:8080/questionanswer/question?operation=getOne&qid="+ qid;
+	
+
+	$(document).on('keyup','#qusId',function(){
+		var id=$('#qusId').val();
+		if(id!=""){
+		var url="http://localhost:8080/questionanswer/question?operation=getOne&qusId="+id;
 		$.ajax({
 			url:url,
-			type:'POST'
+			type:'post'
 		})
 		.done(function(result){
-			result = JSON.parse(result);
-			$("qus").val(result.question);
-			$("an").val(result.answer);
+			result=JSON.parse(result);
+			$("#ques").val(result.question);
+			$("#uname").val(result.userName);
+			$("#date").val(result.qdate);
+
+		});
+		}
+	});
+	$(document).on('click','#getall',function(){
+		var url="/questionanswer/question?operation=getAll";
+		$.ajax({
+			url:url,
+			type:'post'
 		})
-	})
+		.done(function(result){
+			var array=JSON.parse(result);
+			var table="<table border=2px>"
+				table+="<tr><th>Question</th><th>UserName</th><th>QuestionDate</th></tr>";
+			for(i=0;i<array.length;i++){
+				table+="<tr>"
+				table+="<td>"+array[i].question+"</td>";
+				table+="<td>"+array[i].userName+"</td>";
+				table+="<td>"+array[i].qdate+"</td>";
+				table+="</tr>";
+			} 
+			table+="</table>";
+			$(".qus")[0].innerHTML=table;
+		});
+	});
+	
+	
+	
+	$(document).on("click","#sub",function(){
+		var answer=$('#ans').val();
+		var uname=$('#uname').val();
+		var date=$('#adate').val();
+		if(ans==""){
+        alert("please enter your Answer");
+        return;
+		}
+		if(uname==""){
+			alert("please enter your UserName");
+			return;
+		}
+		
+		if(date==""){
+			alert("please enter your Date");
+			return;
+		}
+		var url="/questionanswer/question?operation=addAns&ans="+answer+"&uname="+uname+"&adate="+date;
+		$('input[type=text]').val();
+		$.ajax({
+			url: url,
+			type:'post'
+		})
+		.done(function(result){
+				alert("Successfully Added");
+			})
+		.fail(function(result){
+			alert("Error Occurs");
+		});
+	});
+	
+
+	$(document).on('keyup','#id',function(){
+		var id=$('#id').val();
+		if(id!=""){
+		var url="http://localhost:8080/questionanswer/question?operation=getOne&id="+id;
+		$.ajax({
+			url:url,
+			type:'post'
+		})
+		.done(function(result){
+			result=JSON.parse(result);
+			$("#ans").val(result.answer);
+			$("#uname").val(result.userName);
+			$("#adate").val(result.adate);
+
+		});
+		}
+	});
+	$(document).on('click','#getallans',function(){
+		var url="/questionanswer/question?operation=getAll";
+		$.ajax({
+			url:url,
+			type:'post'
+		})
+		.done(function(result){
+			var array=JSON.parse(result);
+			var table="<table border=2px>"
+				table+="<tr><th>Answer</th><th>UserName</th><th>AnswerDate</th></tr>";
+			for(i=0;i<array.length;i++){
+				table+="<tr>"
+				table+="<td>"+array[i].Answer+"</td>";
+				table+="<td>"+array[i].userName+"</td>";
+				table+="<td>"+array[i].adate+"</td>";
+				table+="</tr>";
+			}
+			table+="</table>";
+			$(".anss")[0].innerHTML=table;
+		});
+	});
 	
 });
+	
+	
