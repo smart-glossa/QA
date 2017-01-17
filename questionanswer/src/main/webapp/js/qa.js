3d375bcafe2ecd12cb1288b7ff44267ce5cf7e24
parameswaris <parameswari@smartglossa.com>
2017-01-17 17:21:14 +0530

    changes

--- questionanswer/src/main/webapp/js/qa.js
+++ questionanswer/src/main/webapp/js/qa.js
@@ -12,10 +12,10 @@ $('.use')[0].innerHTML=user;
 function qa(){
 	var ques="";
 	ques += "<table>";
-	ques += "<tr><td>QuestionId*:<td><td><input type=\"text\"id=\"qusId\"placeholder=\"QuestionId!..\"><\/td><\/tr>";
-	ques += "<tr><td>Question*:<td><td><input type=\"text\"id=\"ques\"placeholder=\"Question!..\"><\/td><\/tr>";
-	ques += "<tr><td>UserName*:<td><td><input type=\"text\"id=\"uname\"placeholder=\"UserName!..\"><\/td><\/tr>";
-	ques += "<tr><td>Date*:<td><td><input type=\"text\"id=\"date\"placeholder=\"DateTime!..\"><\/td><\/tr>";
+	ques += "<tr><td>QuestionId*:<\/td><td><input type=\"text\"id=\"qusId\"placeholder=\"QuestionId!..\"><\/td><\/tr>";
+	ques += "<tr><td>Question*:<\/td><td><textarea type=\"text\"id=\"ques\"placeholder=\"Question!..\"><\/textarea><\/td><\/tr>";
+	ques += "<tr><td>UserName*:<\/td><td><input type=\"text\"id=\"uname\"placeholder=\"UserName!..\"><\/td><\/tr>";
+	ques += "<tr><td>Date*:<\/td><td><input type=\"text\"id=\"date\"placeholder=\"DateTime!..\"><\/td><\/tr>";
 	ques += "<tr><td><button id=\"submit\">Submit<\/button><\/td><\/tr>";
 	ques += "<\/table>";
    $('.question')[0].innerHTML=ques;
@@ -32,3 +32,18 @@ function answer(){
 	ans += "<\/table>";
 $('.answ')[0].innerHTML=ans;
 }
+function questt(){
+	var strVar="";
+	strVar += "<label>What is a HTML:<\/label>";
+	strVar += "	<input type=\"text\" id=\"na\"  \/>";
+	strVar += "	<input type=\"button\" value=\"enter\" onclick=\"diname()\"><br>";
+	strVar += "<p id=\"paraid\"><\/p>";
+	$('.anss')[0].innerHTML=strVar;
+}
+function diname(){
+	text = na.value;
+	document.getElementById("paraid").innerHTML+= '<p>'+text+'<hr>'; 
+document.getElementById("na").value;
+}
+
+
