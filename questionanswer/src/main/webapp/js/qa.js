08489071516654dec04587244f7c28c93fce7ea7
deepikabu <deepika@smartglossa.com>
2017-02-02 17:03:12 -0800

    changes in js file

--- questionanswer/src/main/webapp/js/qa.js
+++ questionanswer/src/main/webapp/js/qa.js
@@ -8,11 +8,6 @@ $(document).ready(function() {
             $("#que").focus().css("outline-color", "#e53935");
             return;
         }
-        if (ans == "") {
-            alert("please enter the answer");
-            $("#ans").focus().css("outline-color", "#e53935");
-            return;
-        }
         var url = "/questionanswer/qa?operation=add&question=" + qus + "&answer=" + ans;
         $.ajax({
                 url: url,
@@ -28,18 +23,30 @@ $(document).ready(function() {
 		$(document).on('click', '#here', function() {
         var qa = "";
         qa += "<center>"
-       qa += "<p>Question</p>";
+       qa += "<div>";
         qa += "<textarea id=\'que\' placeholder=\'enter the question here\'></textarea>";
-        qa += "<div class=\'textarea\'>";
-        qa += "<textarea id=\'ans\' placeholder=\'enter the answer here\'><\/textarea>";
         qa += "<div class=\'post\'>";
         qa += "<button id=\'send\'>POST<\/button>";
         qa += "<\/div>";
         qa += "<\/div>";
-        qa += "<\/center>";
-        $('#qaAddForm')[0].innerHTML = qa;
+ 
+        
+        $('#qaAddForm')[0].innerHTML = qa;  
+        
     });
-});
+		$(document).on('click','.aans',function(){
+			var qa = "";
+			qa += "<div class=\'textarea\'>";
+	        qa += "<textarea id=\'ans\' placeholder=\'enter the answer here\'><\/textarea>";
+	        qa += "<div class=\'post\'>";
+	        qa += "<button id=\'update\'>POST<\/button>";
+	        qa += "<\/div>";
+	        qa += "<\/div>";
+	        qa += "<\/center>";
+	     
+	        $('#typeans')[0].innerHTML=qa;
+		});
+
  $(document).on('click','.home',function(){
     var url = "/questionanswer/qa?operation=getAll";
     $.ajax({
@@ -48,16 +55,41 @@ $(document).ready(function() {
         })
         .done(function(result) {
             var res = JSON.parse(result);
-            var table = "<div>"
+            var table = "<div>";
                 for (var i = 0; i < res.length; i++) {
                 table += "<div class='qa'>";
                 table += "<span>" + res[i].id + "<span>";
                 table += "<b id='question'>" + res[i].question + "</b>";
-                table += "<p id='answer'>" + res[i].answer + "<p>";
+                table += "<p>" +res[i].answer+ "</p>";
+                table += "<button class=\'aans\'>AnswerHere<\/button>";
+                table += "<div id=\'typeans\'><\/div>";
                 table += "</div>";
                 
             }
-            table += "</div>";
+            table += "</div>"
+            	
             $("#getAll")[0].innerHTML = table;
         });
+});
+   
+	    $(document).on('click', '#update', function() {
+	        var qus = $("#que").val();
+	        var ans = $("#ans").val();
+	        if (ans == "") {
+	            alert("please enter the  answer");
+	            $("#que").focus().css("outline-color", "#e53935");
+	            return;
+	        }
+	        var url = "/questionanswer/qa?operation=update&question=" + qus + "&answer=" + ans;
+	        $.ajax({
+	                url: url,
+	                type: 'POST'
+	            })
+	            .done(function(result) {
+	                alert("successfully updated");
+	            })
+	            .fail(function(result) {
+	                alert("Error occurs");
+	            });
+	    });
 });
